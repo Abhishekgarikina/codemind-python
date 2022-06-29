99788dfc519b2c06b0fce5aee9d6f6acae129e99
Abhishekgarikina <98166648+Abhishekgarikina@users.noreply.github.com>
2022-06-29 23:10:02 +0530

    n=int(input()) a=list(map(int,input().split())) s=[] c=0 for i in a:     if i not in s:         s.append(i) for j in s:     if a.count(j)==j:         c=c+1 print(c)

--- value_same_as_count.py
+++ value_same_as_count.py
@@ -1,20 +1,11 @@
 n=int(input())
-arr=list(map(int,input().strip().split()))
-k=[]
-sum=0
-p=0
+a=list(map(int,input().split()))
+s=[]
 c=0
-for i in range(n):
-    if arr[i]!=-1:
-        c=1
-        for j in range(n):
-            if arr[i]==arr[j] and i!=j:
-                c=c+1
-                arr[j]=-1
-        if arr[i]==c:
-            k.append(arr[i])
-for i in range(len(k)):
-    p=p+1
-print(p)    
-            
-            
+for i in a:
+    if i not in s:
+        s.append(i)
+for j in s:
+    if a.count(j)==j:
+        c=c+1
+print(c)
