9e41e099b444a0c38981e773b9a4eb0797698bec
Abhishekgarikina <98166648+Abhishekgarikina@users.noreply.github.com>
2022-06-29 22:26:48 +0530

    def isprime(k):     if k==1:         return False     else:         for j in range(2,int(k**0.5)+1):             if k%j==0:                 return False                 break         else:             return True a=int(input()) b=int(input()) c=0 for i in range(a,b+1):     if isprime(i):         c=c+1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,16 +1,17 @@
-def prime(n):
-    if n==1:
+def isprime(k):
+    if k==1:
         return False
     else:
-        for i in range(2,int(n**0.5)+1):
-            if n%i==0:
+        for j in range(2,int(k**0.5)+1):
+            if k%j==0:
                 return False
+                break
         else:
             return True
-a=int(input())            
-b=int(input())   
+a=int(input())
+b=int(input())
 c=0
 for i in range(a,b+1):
-    if prime(i):
+    if isprime(i):
         c=c+1
 print(c)
