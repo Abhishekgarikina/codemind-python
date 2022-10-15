b19e84c42b59e3343689ef79a91fb129fda265f8
Abhishekgarikina <98166648+Abhishekgarikina@users.noreply.github.com>
2022-10-15 22:33:58 +0530

    x = int(input()) l = list(map(int,input().split())) m=[] n=[] g=[] for i in range(0,len(l),2):     m.append(str(l[i])) for j in range(1,len(l)+1,2):     n.append(l[j]) b = len(m) a=0 if a!=b:     for k in n:         c = m[a]*k         g.append(c)         a+=1 for res in g:     z = list(res)     for t in z:         print(t,end=" ")

--- Generate_array_I.py
+++ Generate_array_I.py
@@ -1,6 +1,20 @@
-n=int(input())
-arr=list(map(int,input().strip().split()))
-for i in range(1,n,2):
-    for j in range(arr[i]):
-        print(arr[i-1],end=" ")
-        
+x = int(input())
+l = list(map(int,input().split()))
+m=[]
+n=[]
+g=[]
+for i in range(0,len(l),2):
+    m.append(str(l[i]))
+for j in range(1,len(l)+1,2):
+    n.append(l[j])
+b = len(m)
+a=0
+if a!=b:
+    for k in n:
+        c = m[a]*k
+        g.append(c)
+        a+=1
+for res in g:
+    z = list(res)
+    for t in z:
+        print(t,end=" ")
