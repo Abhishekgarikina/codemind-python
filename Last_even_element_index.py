0ea1b0d27893a9a93febf57e13adf7715a46d360
Abhishekgarikina <98166648+Abhishekgarikina@users.noreply.github.com>
2022-10-15 22:53:47 +0530

    x = int(input()) l = list(map(int,input().split())) for i in range(x-1,-1,-1):     if l[i]%2==0:         print(i)         break

--- Last_even_element_index.py
+++ Last_even_element_index.py
@@ -1,7 +1,6 @@
-n=int(input())
-a=list(map(int,input().split()))
-for i in range(n-1,-1,-1):
-    if a[i]%2==0:
+x = int(input())
+l = list(map(int,input().split()))
+for i in range(x-1,-1,-1):
+    if l[i]%2==0:
         print(i)
         break
-    
