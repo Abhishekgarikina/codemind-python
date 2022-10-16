c4dae0fbcdc5d8d953819bbd9c943df5f86ca3fa
Abhishekgarikina <98166648+Abhishekgarikina@users.noreply.github.com>
2022-10-16 13:42:06 +0530

    x , y = map(int,input().split()) c=[] d=[] for i in range(x):     l = list(map(int,input().split()))     d.append(sum(l)) print(*d)

--- Row_wise_sum_of_a_matrix.py
+++ Row_wise_sum_of_a_matrix.py
@@ -1,13 +1,7 @@
-m,n=map(int,input().split())
-a=[]
-s=0
-for i in range(m):
-    l=list(map(int,input().split()))
-    if len(l)==n:
-        a.append(l)
-for i in range(m):
-    for j in range(n):
-        s=s+a[i][j]
-    print(s,end=" ")
-    s=0
-        
+x , y = map(int,input().split())
+c=[]
+d=[]
+for i in range(x):
+    l = list(map(int,input().split()))
+    d.append(sum(l))
+print(*d)
