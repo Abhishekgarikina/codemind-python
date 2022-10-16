300efc1b7685a1ad788831fe60241b25e7b86206
Abhishekgarikina <98166648+Abhishekgarikina@users.noreply.github.com>
2022-10-16 13:28:53 +0530

    x = input() s = set(x) if len(x)==len(s):     print(True) else:     print(False)

--- Isogram_or_not.py
+++ Isogram_or_not.py
@@ -1,9 +1,6 @@
-n=input()
-s=''
-for i in n:
-    if i not in s:
-        s=s+i
-if len(n)==len(s):
-    print('True')
+x = input()
+s = set(x)
+if len(x)==len(s):
+    print(True)
 else:
-    print('False')
+    print(False)
