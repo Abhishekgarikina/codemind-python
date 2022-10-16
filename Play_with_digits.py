3c44420663288af7e27c0bdbbb099cb2a9bdff84
Abhishekgarikina <98166648+Abhishekgarikina@users.noreply.github.com>
2022-10-16 10:39:09 +0530

    x = int(input()) l = list(map(str,input().split())) c=0 for i in l:     for j in i:         c+=int(j) print(c)

--- Play_with_digits.py
+++ Play_with_digits.py
@@ -1,9 +1,7 @@
-n=int(input())
-a=list(map(int,input().split()))
-s=0
-for i in range(n):
-    k=str(a[i])
-    for j in k:
-        s=s+int(j)
-print(s)        
-        
+x = int(input())
+l = list(map(str,input().split()))
+c=0
+for i in l:
+    for j in i:
+        c+=int(j)
+print(c)
