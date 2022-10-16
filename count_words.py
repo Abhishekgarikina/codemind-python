22244f78a5c9613738267c6c0ef56c5e0c2ede2d
Abhishekgarikina <98166648+Abhishekgarikina@users.noreply.github.com>
2022-10-16 10:53:11 +0530

    x = list(map(str,input().split())) c=0 l = ['a','e','i','o','u','A','E','I','O','U'] for i in x:     n = len(i)     if (i[0] in l) and (i[n-1] not in l):         c+=1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,15 +1,8 @@
-a=str(input())
-b=a.lower()
-c=b.split()
-count=0
-for i in range(len(c)):
-    m=0
-    s=0
-    d=list(c[i])
-    if d[0]=='a' or d[0]=='e' or d[0]=='i' or d[0]=='o' or d[0]=='u':
-        m=m+1
-    if d[len(d)-1]!='a' and d[len(d)-1]!='e' and d[len(d)-1]!='i' and d[len(d)-1]!='o' and d[len(d)-1]!='u':
-        s=s+1
-    if m+s==2:
-        count=count+1
-print(count)
+x = list(map(str,input().split()))
+c=0
+l = ['a','e','i','o','u','A','E','I','O','U']
+for i in x:
+    n = len(i)
+    if (i[0] in l) and (i[n-1] not in l):
+        c+=1
+print(c)
