0aeb538b08fa5879eec6cf65e701faef85b70db9
Abhishekgarikina <98166648+Abhishekgarikina@users.noreply.github.com>
2022-10-16 13:41:13 +0530

    a,b=map(int,input().split()) l=[] m=[] for i in range(a):     v=list(map(int,input().split()))     m.append(v) i=0 while i!=b:     s=0     for j in m:         s+=j[i]     l.append(s)     i+=1 print(*l)

--- Column_wise_sum_of_a_matrix.py
+++ Column_wise_sum_of_a_matrix.py
@@ -1,8 +1,14 @@
-n,m=map(int,input().split())
-s=[0]*m
-for j in range(n):
-    m=list(map(int,input().split()))
-    for i in range(len(m)):
-        s[i]=s[i]+m[i]
-for k in s:
-    print(k,end=' ')
+a,b=map(int,input().split())
+l=[]
+m=[]
+for i in range(a):
+    v=list(map(int,input().split()))
+    m.append(v)
+i=0
+while i!=b:
+    s=0
+    for j in m:
+        s+=j[i]
+    l.append(s)
+    i+=1
+print(*l)
